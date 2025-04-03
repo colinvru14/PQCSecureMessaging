e622f5df13b81161ebe4262ea966bf54a6378f51
Colin Matti Vrugteman <colin.vrugteman@hotmail.com>
2025-04-02 23:36:38 -0400

    add digital signature

--- src/traditional/receiver.c
+++ src/traditional/receiver.c
@@ -1,6 +1,7 @@
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
+#include <openssl/sha.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -38,12 +39,12 @@ RSA* generate_key_pair() {
 char* get_public_key_PEM(RSA *keypair, size_t *len) {
     BIO *pub = BIO_new(BIO_s_mem());
     PEM_write_bio_RSAPublicKey(pub, keypair);
-    
+
     *len = BIO_pending(pub);
     char *pub_key = malloc(*len + 1);
     BIO_read(pub, pub_key, *len);
     pub_key[*len] = '\0';
-    
+
     BIO_free(pub);
     return pub_key;
 }
@@ -53,64 +54,64 @@ void receiver_process() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    
+
     // Generate RSA key pair
     RSA *keypair = generate_key_pair();
-    
+
     // Get public key in PEM format
     size_t pub_len;
     char *pub_key = get_public_key_PEM(keypair, &pub_len);
     printf("[RECEIVER] Receiver's public key:\n%s\n", pub_key);
-    
+
     // Create socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("[RECEIVER] Socket creation failed");
         exit(EXIT_FAILURE);
     }
-    
+
     // Set socket options
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("[RECEIVER] Setsockopt failed");
         exit(EXIT_FAILURE);
     }
-    
+
     // Set up address
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-    
+
     // Bind socket
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("[RECEIVER] Bind failed");
         exit(EXIT_FAILURE);
     }
-    
+
     // Listen for connections
     if (listen(server_fd, 1) < 0) {
         perror("[RECEIVER] Listen failed");
         exit(EXIT_FAILURE);
     }
     printf("[RECEIVER] Receiver waiting for sender connection on port %d...\n", PORT);
-    
+
     // Accept connection
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("[RECEIVER] Accept failed");
         exit(EXIT_FAILURE);
     }
     printf("[RECEIVER] Sender connected.\n");
-    
+
     // Receive sender's public key
     char sender_pub_key[BUFFER_SIZE];
     int bytes_received = recv(new_socket, sender_pub_key, BUFFER_SIZE, 0);
     sender_pub_key[bytes_received] = '\0';
     printf("[RECEIVER] Received sender's public key:\n%s\n", sender_pub_key);
-    
+
     // Send public key to sender
     send(new_socket, pub_key, pub_len, 0);
     printf("[RECEIVER] Public key sent to sender.\n");
-    
+
     // Convert PEM string to RSA public key
     BIO *bio = BIO_new(BIO_s_mem());
     BIO_puts(bio, sender_pub_key);
@@ -119,39 +120,47 @@ void receiver_process() {
         handle_errors();
         exit(EXIT_FAILURE);
     }
-    
+
     // Loop to keep receiving messages
     char encrypted[BUFFER_SIZE];
+    unsigned char *signature;
     while (1) {
         printf("\n[RECEIVER] Waiting for new message from sender...\n");
+
         // Receive encrypted message
         bytes_received = recv(new_socket, encrypted, BUFFER_SIZE, 0);
-        if (bytes_received <= 0) {
-            if (bytes_received == 0) {
-                printf("[RECEIVER] Sender disconnected.\n");
-            } else {
-                perror("[RECEIVER] Receive failed");
-            }
-            break;
-        }
-        
+        if (bytes_received <= 0) break;
         printf("[RECEIVER] Received encrypted message (%d bytes).\n", bytes_received);
-        
+
+        // Receive signature
+        unsigned int siglen;
+        recv(new_socket, &siglen, sizeof(unsigned int), 0);
+        signature = malloc(siglen);
+        int sig_len = recv(new_socket, signature, siglen, 0);
+        if (sig_len <= 0) break;
+
         // Decrypt message
-        char *decrypted = malloc(bytes_received);
-        int decrypt_len = RSA_private_decrypt(bytes_received, (unsigned char*)encrypted, 
-                                             (unsigned char*)decrypted,
-                                             keypair, RSA_PKCS1_OAEP_PADDING);
+        char decrypted[BUFFER_SIZE];
+        int decrypt_len = RSA_private_decrypt(bytes_received, (unsigned char*)encrypted, (unsigned char*)decrypted, keypair, RSA_PKCS1_OAEP_PADDING);
         if (decrypt_len == -1) {
             handle_errors();
-            free(decrypted);
             continue;
         }
-        
-        printf("[RECEIVER] Decrypted message: %s\n", decrypted);
-        free(decrypted);
+        decrypted[decrypt_len] = '\0';
+
+        // Verify signature
+        if (RSA_verify(NID_sha256, decrypted, strlen(decrypted), signature, siglen, sender_key)) {
+            printf("[RECEIVER] Signature verified! Message: %s\n", decrypted);
+        } else {
+            printf("[RECEIVER] Signature verification failed. Message may be tampered!\n");
+        }
+
+        // Send acknowledgment to sender
+        const char *ack = "ACK";
+        send(new_socket, ack, strlen(ack), 0);
+        printf("[RECEIVER] Sent acknowledgment to sender.\n");
     }
-    
+
     // Clean up
     free(pub_key);
     RSA_free(keypair);
@@ -162,15 +171,10 @@ void receiver_process() {
 }
 
 int main() {
-    // Initialize OpenSSL
     OpenSSL_add_all_algorithms();
     ERR_load_crypto_strings();
-    
     receiver_process();
-    
-    // Clean up OpenSSL
     EVP_cleanup();
     ERR_free_strings();
-    
     return EXIT_SUCCESS;
 }
--- src/traditional/sender.c
+++ src/traditional/sender.c
@@ -1,6 +1,7 @@
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
+#include <openssl/sha.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,11 +9,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define KEY_LENGTH  2048
 #define PUB_EXP     3
 #define PORT        9090
 #define BUFFER_SIZE 4096
+#define LOG_FILE    "sender_timing.log"
 
 // Error handling function
 void handle_errors() {
@@ -38,58 +41,93 @@ RSA* generate_key_pair() {
 char* get_public_key_PEM(RSA *keypair, size_t *len) {
     BIO *pub = BIO_new(BIO_s_mem());
     PEM_write_bio_RSAPublicKey(pub, keypair);
-    
+
     *len = BIO_pending(pub);
     char *pub_key = malloc(*len + 1);
     BIO_read(pub, pub_key, *len);
     pub_key[*len] = '\0';
-    
+
     BIO_free(pub);
     return pub_key;
+}
+
+int sign_message(RSA *keypair, const unsigned char *msg, unsigned int msg_len, unsigned char **signature, size_t *sig_len) {
+    *sig_len = RSA_size(keypair);
+    *signature = malloc(*sig_len);
+
+    if (*signature == NULL) {
+        perror("Memory allocation failed");
+        return 0;
+    }
+
+    if (RSA_sign(NID_sha256, msg, msg_len, *signature, (unsigned int *)sig_len, keypair) != 1) {
+        handle_errors();
+        free(*signature);
+        return 0;
+    }
+
+    return 1;
+}
+
+// Function to log timing information
+void log_timing(const char* message, long long time_taken_ns) {
+    FILE *log_file = fopen(LOG_FILE, "a");
+    if (log_file == NULL) {
+        perror("Failed to open log file");
+        return;
+    }
+
+    time_t now;
+    time(&now);
+    char timestamp[30];
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+    fprintf(log_file, "%lld\n",time_taken_ns);
+    fclose(log_file);
 }
 
 // Sender process function
 void sender_process() {
     int sockfd;
     struct sockaddr_in server_addr;
-    
+
     // Generate RSA key pair
     RSA *keypair = generate_key_pair();
-    
+
     // Get public key in PEM format
     size_t pub_len;
     char *pub_key = get_public_key_PEM(keypair, &pub_len);
     printf("[SENDER] Sender's public key:\n%s\n", pub_key);
-    
+
     // Create socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("[SENDER] Socket creation failed");
         exit(EXIT_FAILURE);
     }
-    
+
     // Set up server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    
+
     // Connect to receiver
     if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("[SENDER] Connection failed");
         exit(EXIT_FAILURE);
     }
     printf("[SENDER] Connected to receiver.\n");
-    
+
     // Send public key to receiver
     send(sockfd, pub_key, pub_len, 0);
     printf("[SENDER] Public key sent to receiver.\n");
-    
+
     // Receive receiver's public key
     char receiver_pub_key[BUFFER_SIZE];
     int bytes_received = recv(sockfd, receiver_pub_key, BUFFER_SIZE, 0);
     receiver_pub_key[bytes_received] = '\0';
     printf("[SENDER] Received receiver's public key:\n%s\n", receiver_pub_key);
-    
+
     // Convert PEM string to RSA public key
     BIO *bio = BIO_new(BIO_s_mem());
     BIO_puts(bio, receiver_pub_key);
@@ -98,23 +136,39 @@ void sender_process() {
         handle_errors();
         exit(EXIT_FAILURE);
     }
-    
+
     // Loop to keep sending messages
-    while (1) {
+    for (int y = 0; y < 25; y++) {
         // Get message to encrypt
         char msg[KEY_LENGTH/8];
         printf("[SENDER] Enter message to encrypt and send (or 'quit' to exit): ");
-        fgets(msg, KEY_LENGTH/8 - 1, stdin);
-        msg[strlen(msg)-1] = '\0';  // Remove newline
+
+        // Start timing
+        struct timespec start, end;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        // fgets(msg, KEY_LENGTH/8 - 1, stdin);
+        // msg[strlen(msg)-1] = '\0';  // Remove newline
+
+        strcpy(msg, "Testing");
         
         if (strcmp(msg, "quit") == 0) {
             printf("[SENDER] Exiting...\n");
             break;
         }
-        
+
+        // Sign the message
+        unsigned char *signature;
+        unsigned int sig_len;
+        if (!sign_message(keypair, (unsigned char*)msg, strlen(msg), &signature, &sig_len)) {
+            printf("[SENDER] Message signing failed.\n");
+            continue;
+        }
+        printf("[SENDER] Message signed successfully.\n");
+
         // Encrypt message with receiver's public key
         char *encrypted = malloc(RSA_size(receiver_key));
-        int encrypt_len = RSA_public_encrypt(strlen(msg)+1, (unsigned char*)msg, 
+        int encrypt_len = RSA_public_encrypt(strlen(msg)+1, (unsigned char*)msg,
                                             (unsigned char*)encrypted,
                                             receiver_key, RSA_PKCS1_OAEP_PADDING);
         if (encrypt_len == -1) {
@@ -122,18 +176,43 @@ void sender_process() {
             free(encrypted);
             continue;
         }
-        
+
         // Send encrypted message
-        int bytes_sent = send(sockfd, encrypted, encrypt_len, 0);
-        if (bytes_sent < 0) {
-            perror("[SENDER] Failed to send message");
-            free(encrypted);
+        send(sockfd, encrypted, encrypt_len, 0);
+        printf("[SENDER] Encrypted message sent.\n");
+
+        // Send signature size
+        send(sockfd, &sig_len, sizeof(unsigned int), 0);
+
+        // Send signature
+        send(sockfd, signature, sig_len, 0);
+        printf("[SENDER] Signature sent (%d bytes).\n", sig_len);
+
+        // Wait for acknowledgment from receiver
+        char ack[4] = {0};
+        int ack_bytes = recv(sockfd, ack, 3, 0);
+
+        // End timing
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        long long time_taken_ns = (end.tv_sec - start.tv_sec) * 1000000000LL +
+                                (end.tv_nsec - start.tv_nsec);
+
+        if (ack_bytes > 0 && strcmp(ack, "ACK") == 0) {
+            printf("[SENDER] Received acknowledgment from receiver.\n");
+            // Log timing information
+            log_timing(msg, time_taken_ns);
+        } else {
+            printf("[SENDER] Error: Did not receive acknowledgment from receiver.\n");
             break;
         }
-        printf("[SENDER] Encrypted message sent (%d bytes).\n", bytes_sent);
+
+        for (int i = 0; i < 0; i++) {}
+        printf("\n");
+
         free(encrypted);
+        free(signature);
     }
-    
+
     // Clean up
     free(pub_key);
     RSA_free(keypair);
@@ -146,12 +225,12 @@ int main() {
     // Initialize OpenSSL
     OpenSSL_add_all_algorithms();
     ERR_load_crypto_strings();
-    
+
     sender_process();
-    
+
     // Clean up OpenSSL
     EVP_cleanup();
     ERR_free_strings();
-    
+
     return EXIT_SUCCESS;
 }
